2bed0e525f685ccdc3aecf23df1fc95337163105
oucwy <dr.yongwang@hotmail.com>
2021-08-01 20:49:18 +0800

    去掉Hello.vue中的循环

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,56 +4,50 @@
     <el-col :span="24" class="toolbar">
       <el-form :inline="true">
         <el-form-item>
-          <el-input placeholder="请输入城市名称" v-model="input2"></el-input>
+          <el-input placeholder="请输入城市名称（拼音）" v-model="input_city" @keydown.native.enter.prevent ="greet"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button v-on:click="greet">查询</el-button>
+          <el-button @click="greet">查询</el-button>
         </el-form-item>
       </el-form>
     </el-col>
-    <el-col :span="24" v-if="show">
-      <el-button type="primary">{{city}}</el-button>
-            <el-button type="primary">{{country}}</el-button>
-
+    <el-col :span="12" :offset="6" v-if="show">
+      <el-tag>{{city}} ({{country}}) 天气</el-tag>
     </el-col>
     <el-col :span="24" v-if="show">
       <el-table
         :data="gridData"
         stripe
         fit
-        style="width: 80% ;text-align: center;">
+        style="width:80%; text-align:center;">
         <el-table-column
           align="center"
-          prop="dt"
           label="日期">
+          <template slot-scope="tbl_data">
+            {{tbl_data.row.dt|toDate}}
+          </template>
         </el-table-column>
         <el-table-column
           align="center"
-          prop="weather.main"
           label="天气">
+          <template slot-scope="tbl_data">
+            <img :src="tbl_data.row.weather[0].icon | toUrl" />
+          </template>
         </el-table-column>
         <el-table-column
           align="center"
-          prop="temp.day"
-          label="日间温度">
+          prop="temp.max"
+          label="最高温度">
         </el-table-column>
         <el-table-column
           align="center"
-          prop="temp.night"
-          label="夜间温度">
+          prop="temp.min"
+          label="最低温度">
         </el-table-column>
         <el-table-column
           align="center"
           prop="pressure"
           label="气压值">
-        </el-table-column>
-        <el-table-column
-          align="center"
-          prop="weather.icon"
-          label="天气情况">
-          <template scope="scope">
-              <img :src="scope.row.weather.icon" />
-          </template>
         </el-table-column>
       </el-table>
     </el-col>
@@ -65,37 +59,45 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'A Simple VUE Weather Demo',
-      input2:null,
-      show:false,
-      gridData:[],
-      city:null,
-      country:null
+      msg: 'VUE天气示例',
+      input_city: null,
+      show: false,
+      gridData: [],
+      city: null,
+      country: null
+    }
+  },
+  filters: {
+    // 把编码转换成天气图标网址
+    toUrl: function (value) {
+      if (!value) return ''
+      return "http://openweathermap.org/img/w/" + value + ".png"
+    },
+    // 把整数值转换成日期
+    toDate: function (value) {
+      var curdate = new Date(value*1000);
+      return `${curdate.getMonth()+1}月${curdate.getDate()}日`;
     }
   },
   methods: {
-      greet: function() {
-						this.$http.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.input2}&mode=json&units=metric&cnt=7&appid=f12159c1f548ea9ab7b5ff1907b1df50`)
-							.then((response) => {
-                this.city=response.data.city.name;
-                this.country=response.data.city.country;
-               this.gridData=response.data.list
-               console.log(this.gridData);
-               for(let i=0;i<this.gridData.length;i++){
-                //  this.gridData[i].img= '<img src="http://openweathermap.org/img/w/'+this.gridData[i].weather[0].icon+'.png">'
-                 this.gridData[i].weather.main=this.gridData[i].weather[0].main  
-                 this.gridData[i].weather.icon= 'http://openweathermap.org/img/w/' + this.gridData[i].weather[0].icon + '.png';                  
-                 var date = new Date(this.gridData[i].dt*1000);
-                 this.gridData[i].dt = `${date.getFullYear()}-${date.getMonth() +1}-${date.getDate()}`
-               } 
-               
-               this.show = true             
-							})
-							.catch(function(response) {
-								console.log(response)
-							})
-					}
+    greet: function() {
+      console.log("greet called.");
+      this.$http.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${this.input_city}&mode=json&units=metric&cnt=7&appid=f12159c1f548ea9ab7b5ff1907b1df50`)
+        .then((response) => {
+          // response.data 对象中包括了返回的全部天气预报数据，包括城市、国家等
+          this.city=response.data.city.name;
+          this.country=response.data.city.country;
+          // data.list 中保存了未来7天的天气数据，包括clouds, temp等
+          this.gridData=response.data.list;
+          console.log("enter remote call.");
+          this.show = true;
+          // console.log(response)
+        })
+        .catch(function(response) {
+          console.log(response)
+        })
     }
+  }
 }
 </script>
 
@@ -130,6 +132,7 @@ export default {
     transform: scale(0);
   }
 }
+
 h1, h2 {
   font-weight: normal;
 }
@@ -147,34 +150,38 @@ li {
 a {
   color: #42b983;
 }
+
 .el-table{
   margin:5% 10%;
 }
+
 .el-row {
     margin-bottom: 20px;
-    
-  }
-  .el-row:last-child {
-      margin-bottom: 0;
-    }
-  .el-col {
+
+}
+
+.el-row:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
     border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
-  .row-bg {
-    padding: 10px 0;
-    background-color: #f9fafc;
-  }
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
